4261b1f85165a4723d68dd25ffb23a6d04153e56
LAB34 <lab34@fatec.com>
2024-09-26 19:39:55 -0300

    primeiro commit

---
+++ src/main/java/com/example/avaliacao_dps_II/AvaliacaoDpsIiApplication.java
@@ -0,0 +1,13 @@
+package com.example.avaliacao_dps_II;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliacaoDpsIiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliacaoDpsIiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/avaliacao_dps_II/AvaliacaoDpsIiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.avaliacao_dps_II;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliacaoDpsIiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
